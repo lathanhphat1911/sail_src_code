9f3f019af489d9067e5087c5a2e57e200997b88a
lathanhphat1911 <5762382@gmail.com>
2026-04-29 18:07:41 +0700

    fix sec

--- src/stories/stories.controller.ts
+++ src/stories/stories.controller.ts
@@ -1,15 +1,21 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UnauthorizedException, Req } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UnauthorizedException, Req, UseGuards } from '@nestjs/common';
 import { StoriesService } from './stories.service';
+
 import { CreateStoryDto } from './dto/create-story.dto';
 import { UpdateStoryDto } from './dto/update-story.dto';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('stories')
 export class StoriesController {
   constructor(private readonly storiesService: StoriesService) {}
 
 
+  @UseGuards(AuthGuard('jwt')) 
   @Get('feed')
   async getMyFeed(@Req() req: any) {
+    
+    console.log("Thông tin Token giải mã được:", req.user);
+
     const userId = req.user?.id || req.user?.sub; 
 
     if (!userId) {
