7fe6b80eb47f24a2412265b6dd8367f9fbc3616c
lathanhphat1911 <5762382@gmail.com>
2026-05-01 22:31:20 +0700

    Added providers in str module

--- src/stories/stories.module.ts
+++ src/stories/stories.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { StoriesService } from './stories.service';
 import { StoriesController } from './stories.controller';
+import { CloudinaryService } from 'src/cloudinary/cloudinary.service';
 
 @Module({
   controllers: [StoriesController],
-  providers: [StoriesService],
+  providers: [StoriesService, CloudinaryService,],
 })
 export class StoriesModule {}
